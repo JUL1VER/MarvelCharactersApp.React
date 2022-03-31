7943b71ce703e671715429971e374cc883792d86
JUL1VER <jul1v3r@gmail.com>
2022-03-31 14:44:00 +0300

    finished!

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import {MainPage, ComicsPage} from '../pages';
+import {MainPage, ComicsPage, Page404, SingleComicPage} from '../pages';
 import AppHeader from "../appHeader/AppHeader";
 
 const App = () => {
@@ -12,14 +12,12 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Switch>
-                        <Route exact path='/'>
-                            <MainPage/>
-                        </Route>
-                        <Route exact path='/comics'>
-                            <ComicsPage/>
-                        </Route>
-                    </Switch>
+                    <Routes>
+                        <Route path='/' element={<MainPage/>}/>
+                        <Route path='/comics' element={<ComicsPage/>}/>
+                        <Route path='/comics/:comicId' element={<SingleComicPage/>}/>
+                        <Route path='*' element={<Page404/>}/>
+                    </Routes>
                 </main>
             </div>
         </Router>
